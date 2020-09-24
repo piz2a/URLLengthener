4662f9cae2092d42b808456dc6a7e27d30194600
piz2a <43025513+piz2a@users.noreply.github.com>
2020-09-24 23:20:27 +0900

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,10 @@
+const params = new URLSearchParams(window.location.search);
+if(params.has('url')) {
+	url = params.get('url');
+	if(!(url.startsWith('http://') || url.startsWith('https://'))) {
+		url += 'http://';
+	}
+	location.href = url;
+} else {
+	document.write('<h3>Invalid URL</h3>');
+}
