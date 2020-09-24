cf6d5cebc9c9b1ce8f23da096e22f53f855190b0
piz2a <43025513+piz2a@users.noreply.github.com>
2020-09-24 23:29:12 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,9 +1,11 @@
 const params = new URLSearchParams(window.location.search);
 if(params.has('url')) {
 	url = params.get('url');
+	/*
 	if(!url.startsWith('http://') && !url.startsWith('https://')) {
 		url = 'http://' + url;
 	}
+	*/
 	location.href = url;
 } else {
 	document.write('<h3>Invalid URL</h3>');
